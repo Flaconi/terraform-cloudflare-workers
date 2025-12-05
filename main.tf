ab1bf8568e75b5909124ef3ada6f90d0ee894aa2
subodh.karwa <subodhkarwa1990@gmail.com>
2025-12-05 10:40:49 +0100

    PLT-1419 Make changes optional

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+locals {
+  routes_enabled = length(var.enabled_routes) > 0 || length(var.disabled_routes) > 0
+  routes_config_valid = var.zone_id != null && var.domain_name != null && var.worker_name != null
+  should_create_routes = local.routes_enabled && local.routes_config_valid
+}
+
 resource "cloudflare_workers_kv_namespace" "this" {
   for_each   = var.kv_database_names
   account_id = var.account_id
@@ -5,14 +11,14 @@ resource "cloudflare_workers_kv_namespace" "this" {
 }
 
 resource "cloudflare_workers_route" "enabled" {
-  for_each = toset(var.enabled_routes)
+  for_each = local.should_create_routes && length(var.enabled_routes) > 0 ? toset(var.enabled_routes) : toset([])
   zone_id  = var.zone_id
   pattern  = format("%s%s", var.domain_name, each.value)
   script   = var.worker_name
 }
 
 resource "cloudflare_workers_route" "disabled" {
-  for_each = toset(var.disabled_routes)
+  for_each = local.should_create_routes && length(var.disabled_routes) > 0 ? toset(var.disabled_routes) : toset([])
   zone_id  = var.zone_id
   pattern  = format("%s%s", var.domain_name, each.value)
   script   = null
--- variables.tf
+++ variables.tf
@@ -12,24 +12,30 @@ variable "kv_database_names" {
 variable "zone_id" {
   description = "Zone ID on cloudflare for the domain"
   type        = string
+  default     = null
 }
 
 variable "domain_name" {
   description = "Domain name"
   type        = string
-}
-
-variable "enabled_routes" {
-  description = "A list of routes enabled for worker"
-  type        = set(string)
+  default     = null
 }
 
 variable "disabled_routes" {
   description = "A list of disabled routes for worker"
   type        = set(string)
+  default     = []
 }
+
+variable "enabled_routes" {
+  description = "A list of routes enabled for worker"
+  type        = set(string)
+  default     = []
+}
+
 
 variable "worker_name" {
   description = "worker name"
   type        = string
+  default     = null
 }
